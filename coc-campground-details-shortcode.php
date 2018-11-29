30618ab47abcc4cfe35d9e012b64f10a27d1f05b
Troy Whitney <42855433+IILLC@users.noreply.github.com>
2018-11-29 14:44:35 -0700

    Add files via upload

---
+++ coc-campground-details-shortcode.php
@@ -0,0 +1,456 @@
+<?php
+
+/*
+Plugin Name: Camp Out Colorado Campground Details + Stocking Report
+Plugin URI: https://www.campoutcolorado.com/
+Description: Adds campground details to reviews. Use [coc_campdetails contract="{contract abrev}" parkid="{facilityID}"] To output just the reservation button link add button="yes" as third attribute. 
+Version: 1.0
+Author: Troy Whitney
+Author URI: https://www.campoutcolorado.com/
+*/
+
+	$whitelist = array(
+						'127.0.0.1',
+						'::1'
+						);
+	
+	if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+		// remote
+		require('/home/campou5/public_html/wp-content/plugins/coc_campground-details/coc-fish-nearby-water-stock-info.php');
+	} else { //local
+		require('E:/Programs/xampp/htdocs/campoutcolorado.com/wp-content/plugins/coc_campground-details/coc-fish-nearby-water-stock-info.php');
+//OLD		require('/coc-fish-nearby-water-stock-info.php');
+	}
+//require('/coc-fish-nearby-water-stock-info.php');
+
+////code to find nearby water
+//function coc_findNearbyWater ($lat, $lon) {
+//	global $wpdb;
+//	if (empty($lat) || empty($lon)) {
+//		return NULL;
+//	} else {
+//		//find water if any
+//		//longitude || cosine(0.4356376777777778)* 69.172 = 1 degree = 62.711413  || Each 0.015946 is 1 mile ||| 5 Miles ==0.079730
+//		//latitude ||  1° ≈ 69 miles || 1 Mile = 0.0144927536231884 || 5 Miles = 0.072463
+//		
+//		$waterSQL = "
+//		Select
+//		  coc_water_details.idcoc_water_details,
+//		  coc_water_details.water_name,
+//		  coc_water_details.lat,
+//		  coc_water_details.lon,
+//		  coc_water_details.fishing_atlas_url,
+//		  coc_water_details.review_url,
+//		  (3959 * acos(cos(radians($lat)) * cos(radians(coc_water_details.lat)) *
+//		  cos(radians(coc_water_details.lon) - radians($lon)) + sin(radians($lat)) *
+//		  sin(radians(coc_water_details.lat)))) As distance
+//		From
+//		  coc_water_details
+//		Group By
+//		  coc_water_details.idcoc_water_details, coc_water_details.water_name,
+//		  coc_water_details.lat, coc_water_details.lon
+//		Having
+//		  distance < 5
+//		Order By
+//		  distance
+//		Limit 0, 5";
+//		$waterResults = $wpdb->get_results($waterSQL);
+//		
+//		//print_r($waterSQL);
+//		//print_r($waterResults);
+//		
+//		if (!empty($waterResults)) {
+//			return $waterResults;
+//		} else {
+//			return NULL;
+//		}
+//	}
+//}
+
+//Register Shortcode: [coc_cg_details]
+add_shortcode( 'coc_campdetails', 'coc_cg_details' );
+
+//example code given at http://developer.active.com/docs/read/Campground_Details_API
+//http://api.amp.active.com/camping/campground/details?contractCode=CO&parkId=50032&api_key=r65cqxbb8phn3c93fvsm7d8
+
+//calback function here
+function coc_cg_details($atts = NULL) { 
+	if (!empty($atts)) {
+		global $wpdb;
+		//contractCode
+		$cCode = $atts['contract'];
+		
+		//Park ID
+		$pID = $atts['parkid'];
+		
+		//code for just button
+		$jButton = $atts['button'];
+		
+		//get coc data 
+		$sql = "
+		Select
+		  coc_campground_details.idcoc_campground_details,
+		  coc_campground_details.coc_campground_name,
+		  coc_campground_details.elevation,
+		  coc_campground_details.campsites,
+		  coc_campground_details.season,
+		  coc_campground_details.lat,
+		  coc_campground_details.lon,
+		  coc_campground_details.driving_directions,
+		  coc_campground_details.amenities,
+		  coc_campground_details.reserve_url,
+		  coc_campground_details.res,
+		  coc_campground_details.camp_updated,
+		  coc_camp_regions.coc_camp_region_name,
+		  coc_camp_regions.coc_camp_regions_lat,
+		  coc_camp_regions.coc_camp_regions_long,
+		  coc_camp_regions.map_url,
+		  coc_forestry.coc_forestry_name,
+		  coc_forestry.coc_forestry_url
+		From
+		  coc_camp_regions Inner Join
+		  coc_campground_details
+			On coc_camp_regions.idcoc_camp_regions =
+			coc_campground_details.region Inner Join
+		  coc_forestry
+			On coc_forestry.idcoc_forestry =
+			coc_campground_details.forestry
+		Where
+		  coc_campground_details.contract_id = '$cCode' And
+		  coc_campground_details.facilityID = $pID";
+		$cocResults = $wpdb->get_row($sql);
+		
+//		print_r($cocResults);
+//		
+//		//debugging
+//		print_r('DATE STAMPS');
+//		print_r("         strtotime('-1 day')= ");
+//		print_r(strtotime('-1 day'));
+//		print_r('     $cocResults->camp_updated=   ');		
+//		print_r($cocResults->camp_updated);
+//		print_r('     strtotime($cocResults->camp_updated)=   ');		
+////		$strtime = $cocResults->camp_updated;
+//		print_r(strtotime($cocResults->camp_updated));
+		
+		if (strtotime('-1 day') > strtotime($cocResults->camp_updated)) {
+			//if longer than 1 day since update, update database fields
+			//print_r("Been longer than 1 day since update. Fire up the API!!!");
+			//my api
+			$activeAPI = 'sr65cqxbb8phn3c93fvsm7d8';
+			//Example API URL CALL   http://api.amp.active.com/camping/campground/details?contractCode=CO&parkId=50032&api_key=sr65cqxbb8phn3c93fvsm7d8
+			$activeURL = 'http://api.amp.active.com/camping/campground/details?contractCode='.$cCode.'&parkId='.$pID.'&api_key='.$activeAPI; //'sr65cqxbb8phn3c93fvsm7d8'
+			//API Call - Only call IF NOT updated within 1 day.
+			$ch = curl_init();
+				curl_setopt($ch, CURLOPT_URL, $activeURL);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+				//curl_setopt($ch, CURLOPT_HEADER, true);
+				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+			$result = curl_exec($ch);
+			
+//// Check if any error occurred
+//if (!curl_errno($ch)) {
+//  $info = curl_getinfo($ch);
+//  echo 'Took = '. $info['total_time'].' seconds to send a request to '. $info['url'] .' which returned with code '. $info['http_code'].' . The content type is '.$info['content_type'].' . The length of the call is '.$info['download_content_length'].' The redirect url is '.$info['redirect_url'].'</br>';
+//} 
+//
+//// Check for errors and display the error message
+//if($errno = curl_errno($ch)) {
+//    $error_message = curl_strerror($errno);
+//    echo "cURL error ({$errno}):\n {$error_message}";
+//}
+//API Error Codes
+//400 - Bad Request
+//403 - Not Authorized
+//403 - Account Inactive
+//403 - Over Queries Per Second Limit
+//403 - Over Rate Limit
+//403 - Unknown Referrer
+//403 - Service Over Queries-per-Second-Limit
+//403 - Service Requires SSL
+//414 - Request URI Too Long
+//502 - Bad Gateway
+//503 - API Maintenance/Service Unavailable
+//504 - Gateway Timeout	
+			
+			curl_close($ch);
+			//parse XML to chop
+			$xml = simplexml_load_string($result);
+
+//MORE DEBUG INFO
+			
+//			print_r("API String ");
+//			echo '<br/>';
+//			print_r($activeURL);	
+//			echo '<br/>';		
+//			print_r("Returned Result= ");
+//			echo '<br/>';
+//			print_r($result);			
+//			echo '<br/>';
+//			print_r("Making API CALL XML RETURN TO FOLLOW= ");
+//			echo '<br/>';
+//			print_r($xml);
+//			echo '<br/>';
+//			print_r("$result is empty= ");
+//			echo '<br/>';
+//			print_r(empty($result));
+//			
+//			echo '<br/>';
+//			print_r("Status code of last cURL call= ");
+//			echo '<br/>';
+//			print_r(empty($result));
+//			echo '<br/>';
+//			print_r("String Length of full resrevation URL= ");
+//			echo '<br/>';
+//			print_r(strlen($xml['fullReservationUrl']));
+//			echo '<br/>';
+//			
+//			print_r(" =End API Call");
+///////////////////////////////////////////////
+		
+			//don't update if no connection/loss of data
+		
+			if  (!empty($xml)) {
+				//have data, check update specific fields
+				$campTable = 'coc_campground_details';
+				$nowDate = date("Y-m-d H:i:s");
+				if (!empty($xml['fullReservationUrl']) && strlen($xml['fullReservationUrl']) > 10){  
+					//update url
+//					print_r("ADDING FULL RESURL==================================================   ");
+//					print_r($xml['fullReservationUrl']);
+//					print_r(" ====================================END FULL RES URL");					
+
+// DO NOT UPDATE URL UNTIL THEY FIX THEIR FUCKING API
+//					$wpdb->update(
+//								$campTable, 
+//								array (
+//									'reserve_url' => $xml['fullReservationUrl'],
+//									'camp_updated' => $nowDate
+//									),
+//								array (
+//									'idcoc_campground_details' => $cocResults->idcoc_campground_details
+//									),
+//								array (
+//									'%s',
+//									'%s'
+//									)
+//								);
+				}
+				
+				//Driving directions Update
+				if (!empty($xml['drivingDirection']) && strlen($xml['drivingDirection']) > 10){
+					$wpdb->update(
+								$campTable, 
+								array (
+									'driving_directions' => $xml['drivingDirection'],
+									'camp_updated' => $nowDate
+									),
+								array (
+									'idcoc_campground_details' => $cocResults->idcoc_campground_details
+									),
+								array (
+									'%s',
+									'%s'
+									)
+								);
+				}
+				
+				//Update Amenities //time to parse out the amenities
+//				if (!empty($xml->amenity)){
+					foreach($xml->amenity as $AMN){
+						$amArray[] = (string)$AMN['name'];
+						$amList = implode(", ",$amArray);
+					}
+					//insert Amenity List if NOT Empty
+					if (strlen($amList) > 5) {
+						$wpdb->update(
+									$campTable, 
+									array (
+										'amenities' => $amList,
+										'camp_updated' => $nowDate
+										),
+									array (
+										'idcoc_campground_details' => $cocResults->idcoc_campground_details
+										),
+									array (
+										'%s',
+										'%s'
+										)
+									);
+					}
+//				}
+				
+				//grab updated info to use
+				$cocResults = $wpdb->get_row($sql);
+				
+			}
+		}
+		
+		//data updated and grabbed - assign vars
+		$cgID = $cocResults->idcoc_campground_details;
+		$res = $cocResults->res;
+		$resURL = $cocResults->reserve_url;
+		$campName = $cocResults->coc_campground_name;
+		$driveDir = $cocResults->driving_directions;
+		$elevation = number_format($cocResults->elevation).' Feet';
+		$forestryName = $cocResults->coc_forestry_name;
+		$forestryURL = $cocResults->coc_forestry_url;
+		$regionName = $cocResults->coc_camp_region_name;
+		$rMapURL = $cocResults->map_url;
+		$campsites = $cocResults->campsites;
+		$season = $cocResults->season;
+		$latLoc = $cocResults->lat;
+		$lonLoc = $cocResults->lon;
+		$amList = $cocResults->amenities;
+		
+		// https://www.google.com/maps/d/viewer?hl=en&authuser=0&mid=1NOd9T5rPujjSZ1h7bQ0CX_x-jEE&z=14&ll=39.319694%2C-106.643564
+		$colMapURL = 'https://www.google.com/maps/d/viewer?mid=1NOd9T5rPujjSZ1h7bQ0CX_x-jEE&z=13&ll='.$latLoc.'%2C'.$lonLoc;
+				
+		//if you can make a resurvation, get the URL and Build Button Stuff
+		if($res == 1) {
+			//	Chapman Test Link	http://www.tantracking.com/click.track?CID=177360&AFID=269137&AffiliateReferenceID=-269137&nonencodedurl=http%3A%2F%2Fwww.reserveamerica.com%2FcampsiteSearch.do%3FcontractCode%3DNRSO%26parkId%3D70422%26cmp=39-19--269137 
+			//builds HTML for reservation button
+			$reserveLink = '<a href="'.$resURL.'" class="btn btn-large btn-block btn-primary" role="button" title="Click here to make a camping reservation." target="_blank">Reserve a Campsite at '.$campName .' Today! <i class="fa fa-external-link"></i></a>';		
+		}
+
+//starts echo of HTML block back to WP
+		ob_start();
+
+		if (!empty($jButton)) {
+			echo $reserveLink;
+		} else {
+	
+			echo $reserveLink;
+	?>		
+			<div>
+				<div class="row-fluid" style="border-bottom: 1px solid #CCC">
+					<div class="span3"><strong>Region:</strong></div>
+					<div class="span9"><a href="<?php echo $rMapURL ?>" title="<?php echo $regionName ?>" target="new" rel="nofollow"><?php echo $regionName ?> <i class="fa fa-external-link"></i></a></div>
+				</div>
+				<div class="row-fluid" style="border-bottom: 1px solid #CCC">
+					<div class="span3"><strong>Coordinates:</strong></div>
+					<div class="span9"><?php echo $latLoc ?>,<?php echo $lonLoc ?></div>
+				</div>
+				<div class="row-fluid" style="border-bottom: 1px solid #CCC">
+					<div class="span3"><strong>Driving Directions:</strong></div>
+					<div class="span9"><?php echo $driveDir ?></div>
+				</div>
+				<div class="row-fluid" style="border-bottom: 1px solid #CCC">
+					<div class="span3"><strong>Map Link:</strong></div>
+					<div class="span9"><a href="<?php echo $colMapURL ?>" title="Camp Out Colorado Camping Map" target="new" rel="nofollow">Camp Out Colorado Camping Map <i class="fa fa-external-link"></i></a></div>
+				</div>
+				<div class="row-fluid" style="border-bottom: 1px solid #CCC">
+					<div class="span3"><strong>Forestry:</strong></div>
+					<div class="span9"><a href="<?php echo $forestryURL ?>" title="<?php echo $forestryName ?>" target="new" rel="nofollow"><?php echo $forestryName ?> <i class="fa fa-external-link"></i></a></div>
+				</div>
+				<div class="row-fluid" style="border-bottom: 1px solid #CCC">
+					<div class="span3"><strong>Elevation:</strong></div>
+					<div class="span9"><?php echo $elevation ?></div>
+				</div>
+				<div class="row-fluid" style="border-bottom: 1px solid #CCC">
+					<div class="span3"><strong>Campsites:</strong></div>
+					<div class="span9"><?php echo $campsites ?></div>
+				</div>
+				<div class="row-fluid" style="border-bottom: 1px solid #CCC">
+					<div class="span3"><strong>Season:</strong></div>
+					<div class="span9"><?php echo $season ?></div>
+				</div>
+				<div class="row-fluid" style="border-bottom: 1px solid #CCC">
+					<div class="span3"><strong>Activities and Amenities:</strong></div>
+					<div class="span9"><?php echo $amList ?></div>
+				</div>
+<?php
+//adding a line if there is any water nearby
+				$localWater = coc_findNearbyWater ($latLoc, $lonLoc);
+				if ($localWater != NULL) { ?>
+						<div class="row-fluid" style="border-bottom: 1px solid #CCC">
+							<div class="span3"><span title="Fishing spots within 5 miles of the campground."><strong>Fishing:</strong></span></div>
+							<div class="span9">
+<?php 
+								foreach ($localWater as $lw) {
+									echo '<a href="'.$lw->fishing_atlas_url.'" target="_blank" ><strong>'.$lw->water_name.'</strong> <i class="fa fa-external-link"></i></a>';
+									echo '  ';
+									echo coc_stocked_lvl($lw->idcoc_water_details);
+									echo '&nbsp &nbsp &nbsp';
+									if (!empty($lw->review_url)){
+										echo '<i class="fa fa-hand-o-right" aria-hidden="true"></i> <a href="..'.$lw->review_url.'">Read our camping review! <i class="fa fa-file-text-o"></i></a>';
+									}
+									echo '</br >';
+								} ?>
+							</div>
+						</div>
+<?php
+				}
+//// lets find campgrounds that are close
+////funciton to return nearby campgrounds NOT the one listed
+//function coc_findNearbyCamps ($cgID, $latLoc, $lonLoc) {
+//	global $wpdb;
+//	$sql = "
+//	SELECT
+//	  coc_campground_details.idcoc_campground_details,
+//	  coc_campground_details.coc_campground_name,
+//	  coc_campground_details.reserve_url,
+//	  coc_campground_details.review_url,
+//	  (3959 * acos(cos(radians($latLoc)) * cos(radians(coc_campground_details.lat)) *
+//	  cos(radians(coc_campground_details.lon) - radians($lonLoc)) + sin(radians($latLoc)) *
+//	  sin(radians(coc_campground_details.lat)))) AS distance
+//	FROM
+//	  coc_campground_details
+//	WHERE
+//		coc_campground_details.idcoc_campground_details != '$cgID' 
+//	Having
+//			  distance < 15
+//			Order By
+//			  distance
+//			Limit 0, 5";
+//	$campResults = $wpdb->get_results($sql);
+////	print_r("$sql= </br>");
+////	print_r($sql);
+////	echo "</br>";
+////	print_r("$campResults= </br>");
+////	print_r($campResults);
+////	echo "</br>";
+//	
+//	
+//	if (!empty($campResults)) {
+//		return $campResults;
+//	} else {
+//		return NULL;
+//	}
+//}
+
+				$localCampgrounds = coc_findNearbyCamps ($cgID, $latLoc, $lonLoc);
+				if ($localCampgrounds != NULL) { ?>
+						<div class="row-fluid" style="border-bottom: 1px solid #CCC">
+							<div class="span3"><span title="Other campgrounds within 10 miles." ><strong>More Camping:</strong></span></div>
+							<div class="span9">
+<?php 
+								foreach ($localCampgrounds as $lc) {
+									echo '<a href="'.$lc->reserve_url.'" target="_blank" ><strong>'.$lc->coc_campground_name.'</strong> <i class="fa fa-external-link"></i></a>';
+									echo '&nbsp &nbsp &nbsp';
+									if (!empty($lc->review_url)){
+										echo '<i class="fa fa-hand-o-right" aria-hidden="true"></i> <a href="..'.$lc->review_url.'">Read our camping review! <i class="fa fa-file-text-o"></i></a>';
+									}
+									echo '</br >';
+								} 
+?>
+							</div>
+						</div>
+<?php
+				}
+?>                    
+                
+                
+			</div>
+	<?php 	echo $reserveLink;
+		}
+
+		return ob_get_clean();
+		
+	} else {
+		return 'No Park Identified';		
+	}
+}
+
+
+?>
